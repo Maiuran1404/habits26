239ea5d45eb9d4f919ee18a833bf16a01eb1b4b0
Maiuran1404 <maiuran.loganathan@uni.minerva.edu>
2025-12-30 15:13:17 +0100

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-import dynamic from "next/dynamic";
 import HabitTracker from "@/components/HabitTracker";
-
-// Lazy load AuthModal - only loaded when auth modal is opened
-const AuthModal = dynamic(() => import("@/components/AuthModal"), {
-  ssr: false, // Don't render on server since it's a client-only modal
-});
+import AuthModal from "@/components/AuthModal";
 
 export default function Home() {
   return (
